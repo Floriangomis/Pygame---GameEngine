d905d29de34ee361fa33360cbb76113bc92c5084
Florian Gomis <Florian.Gomis@viacesi.fr>
2013-10-02 18:56:36 +0200

    Ajout de la classe Perso

---
+++ perso.py
@@ -0,0 +1,35 @@
+import pygame
+class Perso(object):
+
+	def __init__(self, img_droite, img_gauche, img_haut, img_bas):
+		super(Perso, self).__init__()
+
+		# On charge les images pour les differentes positions
+		self.img_droite = pygame.image.load(img_droite).convert_alpha()
+		self.img_gauche = pygame.image.load(img_gauche).convert_alpha()
+		self.img_haut = pygame.image.load(img_haut).convert_alpha()
+		self.img_bas = pygame.image.load(img_bas).convert_alpha()
+
+		#Position du personnage en cases et en pixels
+		self.x = 100
+		self.y = 100
+
+		#Direction par defaut
+		self.direction = self.img_droite 
+
+	def deplacer(self, direction):
+		if direction == "droite":
+			self.direction = self.img_droite
+			self.x = self.x+1
+
+		if direction == "gauche":
+			self.direction = self.img_gauche
+			self.x = self.x-1
+
+		if direction == "haut":
+			self.direction = self.img_haut
+			self.y = self.y-1
+
+		if direction == "bas":
+			self.direction = self.img_bas
+			self.y = self.y+1
