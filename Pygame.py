90bf7847aa94f171a682c9605dbd03ad48eb908f
Florian Gomis <Florian.Gomis@viacesi.fr>
2013-10-02 13:07:43 +0200

    First commit

---
+++ Pygame.py
@@ -0,0 +1,21 @@
+import pygame
+from pygame.locals import *
+
+pygame.init()
+
+fenetre = pygame.display.set_mode((640, 480))
+
+fond = pygame.image.load("background.jpg").convert()
+fenetre.blit(fond,(0,0))
+
+perso = pygame.image.load("perso.png").convert_alpha()
+fenetre.blit(perso, (10,200))
+
+pygame.display.flip()
+
+continuer = 1
+
+while continuer:
+	for event in pygame.event.get():
+                if event.type == QUIT:
+                        continuer = 0
