770a413a1ea66dab2c8fa664e248ba6cdeda7db5
Florian Gomis <Florian.Gomis@viacesi.fr>
2013-10-04 11:15:09 +0200

    Ajout de la classse TileMap qui gere les tiles tirée d'un tileset

---
+++ tilemap.py
@@ -0,0 +1,22 @@
+import pygame
+from pygame.locals import *
+
+class tileMap(object):
+
+	def __init__(self, tileset):
+		super(tileMap, self).__init__()
+		self.tileset = tileset
+	
+	def load_tile_table(self, width, height):
+		image = pygame.image.load(self.tileset).convert_alpha()
+		image_width, image_height = image.get_size()
+
+		tile_table = []
+
+		for tile_x in range(0, image_width/width):
+			line = []
+			tile_table.append(line)
+			for tile_y in range(0, image_height/height):
+				rect = (tile_x*width, tile_y*height, width, height)
+				line.append(image.subsurface(rect))
+		return tile_table
