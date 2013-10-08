214d9104603f5f01a1ba5d09e4871ce093d558b5
Florian Gomis <Florian.Gomis@viacesi.fr>
2013-10-08 16:48:15 +0200

    Optimisation de la classe principale (Gestion d'event du personnage et d'affichage du personnage -> dans la classe personnage)

--- Pygame.py
+++ Pygame.py
@@ -1,78 +1,56 @@
 import pygame
 import sys, os
+from tileset import tileSet
 from tilemap import tileMap
 from cursor import Cursor
+from map import Map
 from menu import Menu
 import settings as settings
 from perso import Perso
 from pygame.locals import *
 
+# Initialisation de l objet Pygame et Creation de la surface sur laquelle on va dessiner les element
 pygame.init()
-
-BLACK = 0, 0, 0
 fenetre = pygame.display.set_mode((800, 600))
-
 #Creation des perso Pj et Pnj
-perso = Perso("./ressources/images/dk_droite.png","./ressources/images/dk_gauche.png","./ressources/images/dk_haut.png","./ressources/images/dk_bas.png", 0, 0)
-perso2 = Perso("./ressources/images/dk_droite.png","./ressources/images/dk_gauche.png","./ressources/images/dk_haut.png","./ressources/images/dk_bas.png",100,100)
-perso3 = Perso("./ressources/images/dk_droite.png","./ressources/images/dk_gauche.png","./ressources/images/dk_haut.png","./ressources/images/dk_bas.png",200,100)
+perso = Perso("./ressources/images/dk_droite.png","./ressources/images/dk_gauche.png","./ressources/images/dk_haut.png","./ressources/images/dk_bas.png", 200, 100, fenetre)
 
-liste = [perso2, perso3]
 
 #On charge la carte en memoire puis on cree un tableau qui contient les case de l image.
-tileMap = tileMap("./ressources/images/tileset.png")
-tile = tileMap.load_tile_table(48,48)
+tileSet = tileSet("./ressources/images/tileset.png")
+tileSet.load_tile_table(48,48)
+dicoTile = tileSet.genererDicoTile()
+# Creation du tableau qui represente la map
+map = Map('level.lvl')
+Matrix = map.getMyArrayMap()
+# Creation de l objet TileMap ou on va gerer le rendu de la map
+tileMap = tileMap(Matrix, dicoTile, fenetre)
 
+
+#LA BOUCLE PRINCIPALE DEMARRE ICI 
 continuer = 1
 mon_horloge = pygame.time.Clock()
-
-# On recupere dans le fichier Level le niveau puis on construit un tableau a deux dimension
-Matrix = [[0 for x in xrange(36)] for x in xrange(16)] 
-h=0
-f = open('level.lvl','r')
-for line in f:
-	h = h+1
-	for x in xrange(0,len(line)):
-		if line[x] == '0' or line[x] == '1':
-			Matrix[h][x] = line[x]
-f.close()
-
-
 while continuer:
 	for event in pygame.event.get():
 		if event.type == QUIT:
 			continuer = 0
+
 	keys = pygame.key.get_pressed()
+
 	if keys[K_ESCAPE]:
 		menu = Menu(fenetre)
 		menu.open_menu()
 
-	if keys[K_RIGHT]:
-		if perso.position_perso.colliderect(perso2.setPosition(-3,0))!=1:
-			perso.deplacer("droite", fenetre)
-	if keys[K_LEFT]:
-		if perso.position_perso.colliderect(perso2.setPosition(3,0))!=1:
-			perso.deplacer("gauche", fenetre)
-	if keys[K_UP]:
-		if perso.position_perso.colliderect(perso2.setPosition(0,3))!=1:
-			perso.deplacer("haut", fenetre)
-	if keys[K_DOWN]:
-		if perso.position_perso.colliderect(perso2.setPosition(0,-3))!=1: 
-			perso.deplacer("bas", fenetre)
 
 	# Remplace le fond
 	fenetre.fill(pygame.Color("Black"))
+	#Rendu de la map
+	tileMap.mapRender()
+	#On gere le Perso Ici (Mouvement et affichage a l ecran)
+	perso.event()
 
-	for width in range(len(Matrix)):
-		for height in range(len(Matrix[width])):
-			if Matrix[width][height] == '0':
-				fenetre.blit(tile[1][4], (height*48, (width-1)*48))
-			if Matrix[width][height] == '1':
-				fenetre.blit(tile[3][4], (height*48, (width-1)*48))
 
-	fenetre.blit(perso.direction, perso.position_perso)
-	fenetre.blit(perso2.direction, perso2.position_perso)
-	fenetre.blit(perso3.direction, perso3.position_perso)
+
 
 	pygame.display.flip()
 	mon_horloge.tick(60)
