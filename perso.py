9282680f7daec14292e6b3b8594d0912afe01faf
Florian Gomis <Florian.Gomis@viacesi.fr>
2013-10-08 16:49:18 +0200

    Optimisation de la classe qui inclue desormais l'affichage et les events

--- perso.py
+++ perso.py
@@ -3,8 +3,10 @@ from pygame.locals import *
 
 class Perso(object):
 
-	def __init__(self, img_droite, img_gauche, img_haut, img_bas, positionX, positionY):
+	def __init__(self, img_droite, img_gauche, img_haut, img_bas, positionX, positionY, fenetre):
 		super(Perso, self).__init__()
+
+		self.screen = fenetre
 
 		# On charge les images pour les differentes positions
 		self.img_droite = pygame.image.load(img_droite).convert_alpha()
@@ -13,36 +15,35 @@ class Perso(object):
 		self.img_bas = pygame.image.load(img_bas).convert_alpha()
 
 		#Direction par defaut
-		self.direction = self.img_droite 
+		self.direction = self.img_droite
 
 		#Position du personnage en cases et en pixels
 		self.position_perso = self.direction.get_rect()
 		self.position_perso = self.position_perso.move(positionX, positionY)
 
+	def event(self):
+		for event in pygame.event.get():
+			if event.type == QUIT:
+				continuer = 0
 
-	def deplacer(self, direction, fenetre):
-		if direction == "droite":
+		keys = pygame.key.get_pressed()
+
+		if keys[K_RIGHT]:
 			self.direction = self.img_droite
 			self.position_perso = self.position_perso.move(3,0)
-			fenetre.blit(self.direction, self.position_perso)
-
-
-		if direction == "gauche":
+			self.screen.blit(self.direction, self.position_perso)
+		if keys[K_LEFT]:
 			self.direction = self.img_gauche
 			self.position_perso = self.position_perso.move(-3,0)
-			fenetre.blit(self.direction, self.position_perso)
-
-
-		if direction == "haut":
+			self.screen.blit(self.direction, self.position_perso)
+		if keys[K_UP]:
 			self.direction = self.img_haut
 			self.position_perso = self.position_perso.move(0,-3)
-			fenetre.blit(self.direction, self.position_perso)
-
-		if direction == "bas":
+			self.screen.blit(self.direction, self.position_perso)
+		if keys[K_DOWN]:
 			self.direction = self.img_bas
 			self.position_perso = self.position_perso.move(0,3)
-			fenetre.blit(self.direction, self.position_perso)
-
-	def setPosition(self, positionX, positionY):
-		 return self.position_perso.move(positionX, positionY)
-		  
+			self.screen.blit(self.direction, self.position_perso)
+		
+		#met a jour la position du perso
+		self.screen.blit(self.direction, self.position_perso)
