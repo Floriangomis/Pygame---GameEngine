e6bd65eccf300bc8020da34565f3b9bafaed89fe
Florian Gomis <Florian.Gomis@viacesi.fr>
2013-10-15 13:48:21 +0200

    Prise en charge de plusieurs layer

--- tilemap.py
+++ tilemap.py
@@ -4,31 +4,33 @@ from tile import Tile
 
 class tileMap(object):
 	"""docstring for tileMap"""
-	def __init__(self, arrayMap, dicoTile, fenetre):
+	def __init__(self, listMatrix, dicoTile, fenetre):
 		super(tileMap, self).__init__()
-		self.tileMap = arrayMap
+		self.tileMap = listMatrix
 		self.dicoTile = dicoTile
 		self.screen = fenetre
 		
-		for height in range(len(self.tileMap)):
-			for width in range(len(self.tileMap[height])):
-				if self.tileMap[height][width] == '0':
-					surfaceC = self.dicoTile[0].getSurface().copy()
-					tile = Tile(False, surfaceC)
-					self.tileMap[height][width] = tile
-				elif self.tileMap[height][width] == '1':
-					surfaceC = self.dicoTile[1].getSurface().copy()
-					tile = Tile(True, surfaceC)
-					self.tileMap[height][width] = tile
-				elif self.tileMap[height][width] == '4':
-					surfaceC = self.dicoTile[4].getSurface().copy()
-					tile = Tile(False, surfaceC)
-					self.tileMap[height][width] = tile		
+		for x in range(len(self.tileMap)):
+			for height in range(len(self.tileMap[x])):
+				for width in range(len(self.tileMap[x][height])):
+					if self.tileMap[x][height][width] == '0':
+						surfaceC = self.dicoTile[0].getSurface().copy()
+						tile = Tile(False, surfaceC)
+						self.tileMap[x][height][width] = tile
+					elif self.tileMap[x][height][width] == '1':
+						surfaceC = self.dicoTile[1].getSurface().copy()
+						tile = Tile(True, surfaceC)
+						self.tileMap[x][height][width] = tile
+					elif self.tileMap[x][height][width] == '4':
+						surfaceC = self.dicoTile[4].getSurface().copy()
+						tile = Tile(False, surfaceC)
+						self.tileMap[x][height][width] = tile		
 
 	def mapRender(self):
-		for height in range(len(self.tileMap)):
-			for width in range(len(self.tileMap[height])-1):
-				self.tileMap[height][width].draw(self.screen, self.tileMap[height][width].setPositionAndGet(width*48, height*48))
+		for x in range(len(self.tileMap)):
+			for height in range(len(self.tileMap[x])):
+				for width in range(len(self.tileMap[x][height])):
+					self.tileMap[x][height][width].draw(self.screen, self.tileMap[x][height][width].setPositionAndGet(width*48, height*48))
 
 	def getTile(self, posY, posX):
-		return self.tileMap[posY/48][posX/48]
+		return self.tileMap[1][posY/48][posX/48]
