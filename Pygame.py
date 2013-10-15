2f90f0dafb1eaadcffdc2b5d027e4eabe087a67b
Florian Gomis <Florian.Gomis@viacesi.fr>
2013-10-16 01:15:41 +0200

    Integration de la camera

--- Pygame.py
+++ Pygame.py
@@ -8,7 +8,9 @@ from menu import Menu
 import settings as settings
 from perso import Perso
 from npc import Npc
+from camera import Camera
 from pygame.locals import *
+
 
 # Initialisation de l objet Pygame et Creation de la surface sur laquelle on va dessiner les element
 pygame.init()
@@ -17,8 +19,6 @@ fenetre = pygame.display.set_mode((800, 600))
 #Creation des perso Pj et Pnj
 perso = Perso("./ressources/images/dk_droite.png","./ressources/images/dk_gauche.png","./ressources/images/dk_haut.png","./ressources/images/dk_bas.png", 400, 130, fenetre)
 Npc = Npc("./ressources/images/dk_bas.png", 540, 250, fenetre, perso)
-
-
 #On charge la carte en memoire puis on cree un tableau qui contient les case de l image.
 tileSet = tileSet("./ressources/images/tileset.png")
 tileSet.load_tile_table(48,48)
@@ -28,13 +28,15 @@ map = Map('level.lvl')
 Matrix = map.getMyArrayMap()
 map2 = Map('level2.lvl')
 Matrix2 = map2.getMyArrayMap()
-
 listMatrix = []
 listMatrix.append(Matrix)
 listMatrix.append(Matrix2)
-
 # Creation de l objet TileMap ou on va gerer le rendu de la map
 tileMap = tileMap(listMatrix, dicoTile, fenetre)
+# Creation de l objet camera qui va nous permettre de fixer la vue sur le hero
+camera = Camera(800, 600)
+
+
 #LA BOUCLE PRINCIPALE DEMARRE ICI 
 continuer = 1
 mon_horloge = pygame.time.Clock()
@@ -49,12 +51,13 @@ while continuer:
 		menu = Menu(fenetre)
 		menu.open_menu()
 
+	fenetre.fill(Color("Black"))
 	#Rendu de la map
-	tileMap.mapRender()
-
+	tileMap.mapRender(camera)
+	camera.update(perso)
 	#On gere le Perso Ici (Mouvement et affichage a l ecran)
-	perso.event(tileMap)
-	Npc.event(tileMap)
+	perso.event(tileMap, camera)
+	Npc.event(tileMap, camera)
 
 	#Souris position
 	#mouse_pos = pygame.mouse.get_pos()
